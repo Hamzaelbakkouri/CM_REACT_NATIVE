a3e8f39e8d1ee24cb51713217b187246e9ab2088
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2024-04-15 11:56:48 +0100

    setup app

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import {View,Image,Text,ImageBackground} from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import React from 'react';
+import { TouchableOpacity} from 'react-native-gesture-handler';
+import "react-native-gesture-handler";
+
+
+export default function App(): React.JSX.Element  {
+
+  return (
+    <>
+      <GestureHandlerRootView>
+      
+    </GestureHandlerRootView>
+    </>
+  );
+}
